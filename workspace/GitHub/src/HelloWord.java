c889dbd52191766c3afb6fdd3aabe704e98a5fe7
A15216350 <A15216350@FMAT-CC5B41.inet.uady.mx>
2016-03-02 10:12:51 -0600

    Cambios

--- workspace/GitHub/src/HelloWord.java
+++ workspace/GitHub/src/HelloWord.java
@@ -3,7 +3,7 @@ public class HelloWord {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("GitHub");
+		System.out.println("Probando");
 		
 
 	}
